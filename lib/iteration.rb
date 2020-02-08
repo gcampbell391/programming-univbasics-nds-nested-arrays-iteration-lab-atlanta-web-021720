c510fcb50b1b6b9835f7ebfc3b50c385d9e1139b
Gene Campbell III <gcampbell391@gmail.com>
2020-02-08 19:36:01 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -4,8 +4,7 @@ def join_ingredients(src)
  while row_index < src.count do 
    element1 = 0
    element2 = 1
-    pizza_Strings.push("I love #{src[row_index][element_index]} and #{src[row_index][element2_index]} on my pizza")
-  end
+    pizza_Strings.push("I love #{src[row_index][element1]} and #{src[row_index][element2]} on my pizza")
   row_index += 1
 end
 pizza_Strings
