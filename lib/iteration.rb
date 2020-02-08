18e4314231693a855f593f629593d10b23af9291
Gene Campbell III <gcampbell391@gmail.com>
2020-02-08 20:14:25 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -28,11 +28,11 @@ end
 
 def total_even_pairs(src)
   row_index = 0 
+  total_evens_sum = 0
   inner_array = []
   while row_index < src.count do 
   element1 = 0
   element2 = 1
-  total_evens_sum = 0
   if src[row_index][element1]%2 == 0 && src[row_index][element2]%2 == 0
     total_evens_sum += src[row_index][element1] + src[row_index][element2]
   end
